747a36d74ac12da07017aab31cd3052dc5dcd1b3
Kinan Mohrez <kinanmohrez2015@gmail.com>
2022-08-15 11:39:53 +0200

    first part of Backend

---
+++ Backend/server/app.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+		secret: 'LightBlog',
+		cookie: { maxAge: 60000 },
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+if (!isProduction) {
+	app.use(errorHandler());
+}
+
+mongoose.connect(process.env.MONGO_URL, {
+	user: process.env.MONGO_USER,
+	pass: process.env.MONGO_PASS,
+});
+
+mongoose.connection.on('connected', () => {
+	console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.error(err);
+});
+
+mongoose.set('debug', true);
+
+// Add models
+require('./models/Articles');
+// Add routes
+app.use(require('./routes'));
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+if (!isProduction) {
+	app.use((err, req, res) => {
+		res.status(err.status || 500);
+
+		res.json({
+			errors: {
+				message: err.message,
+				error: err,
+			},
+		});
+	});
+}
+
+app.use((err, req, res) => {
+	res.status(err.status || 500);
+
+	res.json({
+		errors: {
+			message: err.message,
+			error: {},
+		},
+	});
+});
+
+const server = app.listen(8000, () =>
+	console.log('Server started on http://localhost:8000')
+);
